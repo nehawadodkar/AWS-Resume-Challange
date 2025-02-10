f708acc4bd8d18e7774a71e63fdee1993f356ca7
Neha Wadodkar <neha.wadodkar@omf.com>
2025-02-10 13:20:24 -0600

    added terraform plan on merge request

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "nehaw-terraform-backend"
     key            = "aws-resume-challange/terraform.tfstate"   # Path within the bucket
     region         = "us-east-1"                   # Replace with your region
-    #dynamodb_table = "terraform-lock"              # For state locking (optional)
+    ##dynamodb_table = "terraform-lock"              # For state locking (optional)
     encrypt        = true                          # Encrypt the state at rest
   }
 }
